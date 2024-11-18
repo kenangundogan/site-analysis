60d9d32e9aee4040777b4e688e3f077a97424ff6
Kenan Gündoğan <19374764+kenangundogan@users.noreply.github.com>
2024-11-18 11:44:04 +0300

    refactor link extraction to ensure unique internal URLs and handle invalid links

--- src/utils/linkUtils.js
+++ src/utils/linkUtils.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { JSDOM } from 'jsdom';
 import Link from '../models/link.js';
 import getRandomHeader from './headerSelector.js';
+import { URL } from 'url';
 
 const fetchLinkStatusAndUpdateDB = async (link, scanUuid, options, headerType) => {
     const startTime = new Date();
@@ -65,18 +66,38 @@ const extractLinks = (html, baseUrl) => {
     const document = dom.window.document;
     const links = Array.from(document.querySelectorAll('a'));
 
+    const uniqueUrls = new Set(); // Benzersiz URL'leri takip etmek için Set kullanıyoruz
+    const baseDomain = new URL(baseUrl).hostname; // Temel alan adını alıyoruz
+
     return links
         .map((link) => {
             const href = link.getAttribute('href');
             const alt = link.textContent.trim();
             if (href && !href.startsWith('#') && !href.startsWith('javascript')) {
-                return {
-                    alt,
-                    url: href.startsWith('/') ? `${baseUrl}${href}` : href,
-                };
+                try {
+                    // Href'i mutlak bir URL'ye dönüştürüyoruz
+                    const absoluteUrl = new URL(href, baseUrl).href;
+                    const urlDomain = new URL(absoluteUrl).hostname;
+
+                    // Sadece iç URL'leri ekliyoruz
+                    if (urlDomain === baseDomain) {
+                        // Benzersizliği kontrol ediyoruz
+                        if (!uniqueUrls.has(absoluteUrl)) {
+                            uniqueUrls.add(absoluteUrl);
+                            return {
+                                alt,
+                                url: absoluteUrl,
+                            };
+                        }
+                    }
+                } catch (e) {
+                    // Geçersiz URL durumunda hiçbir şey yapmıyoruz
+                    return null;
+                }
             }
+            return null;
         })
-        .filter((link) => link);
+        .filter((link) => link !== null); // Null olmayan değerleri alıyoruz
 };
 
 export default {
