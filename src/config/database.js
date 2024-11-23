4fc8a22e4db31d273bf8c108ab2e2e5269aa8318
Kenan Gündoğan <19374764+kenangundogan@users.noreply.github.com>
2024-11-23 18:21:40 +0300

    feat(database): configure MongoDB connection using environment variables

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
+    const { MONGO_HOST, MONGO_PORT, MONGO_DB, MONGO_USER, MONGO_PASS, MONGO_AUTH_SOURCE } = process.env;
+    const mongoURI = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}?authSource=${MONGO_AUTH_SOURCE}`;
+
     try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB ${process.env.MONGO_URI} üzerinde çalışıyor.`);
+        await mongoose.connect(mongoURI);
+        console.log(`MongoDB ${MONGO_HOST}:${MONGO_PORT} üzerinde çalışıyor.`);
     } catch (error) {
         console.error('MongoDB bağlantı hatası:', error.message);
-        process.exit(1);
+        process.exit(1);    
     }
 };
 
